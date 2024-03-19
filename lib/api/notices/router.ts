8688b307c16df0ab459ddfa1382035ae326c3d0b
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-19 19:17:00 +0900

    more apis

--- lib/api/notices/router.ts
+++ lib/api/notices/router.ts
@@ -2,13 +2,27 @@ import { z } from 'zod'
 
 import { NoticePostSchema } from '@/lib/prisma'
 
-import { protectedProcedure, router } from '../trpc/init'
+import { protectedProcedure, publicProcedure, router } from '../trpc/init'
 
 export const notices = router({
   create: protectedProcedure
     .input(z.object({}))
-    .output(z.array(NoticePostSchema.pick({ id: true })))
+    .output(NoticePostSchema.pick({ id: true }))
     .mutation(async ({ input, ctx }) => {
+      throw new Error('Not implemented')
+    }),
+
+  list: publicProcedure
+    .input(z.object({ offset: z.number() }))
+    .output(z.array(NoticePostSchema.pick({ id: true })))
+    .query(async ({ input, ctx }) => {
+      throw new Error('Not implemented')
+    }),
+
+  get: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .output(NoticePostSchema)
+    .query(async ({ input, ctx }) => {
       throw new Error('Not implemented')
     })
 })
