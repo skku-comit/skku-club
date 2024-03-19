58253987324cf4d5b41f8a294b430c527cb2dcd9
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-19 19:59:44 +0900

    router

--- lib/api/notices/router.ts
+++ lib/api/notices/router.ts
@@ -1,6 +1,7 @@
+import { TRPCError } from '@trpc/server'
 import { z } from 'zod'
 
-import { NoticePostSchema } from '@/lib/prisma'
+import { db, NoticePostSchema } from '@/lib/prisma'
 
 import { protectedProcedure, publicProcedure, router } from '../trpc/init'
 
@@ -14,15 +15,47 @@ export const notices = router({
 
   list: publicProcedure
     .input(z.object({ offset: z.number() }))
-    .output(z.array(NoticePostSchema.pick({ id: true })))
-    .query(async ({ input, ctx }) => {
-      throw new Error('Not implemented')
+    .output(
+      z.array(
+        NoticePostSchema.pick({ id: true, title: true, publishedAt: true })
+      )
+    )
+    .query(async ({ input: { offset }, ctx }) => {
+      const notices = await db.noticePost.findMany({
+        skip: offset,
+        take: 10,
+        orderBy: { publishedAt: 'desc' },
+        select: {
+          id: true,
+          title: true,
+          publishedAt: true
+        }
+      })
+
+      return notices
     }),
 
   get: publicProcedure
     .input(z.object({ id: z.number() }))
-    .output(NoticePostSchema)
-    .query(async ({ input, ctx }) => {
-      throw new Error('Not implemented')
+    .output(NoticePostSchema.pick({ id: true, title: true, content: true }))
+    .query(async ({ input: { id }, ctx: { user } }) => {
+      const notice = await db.noticePost.findUnique({
+        where: { id },
+        select: {
+          id: true,
+          title: true,
+          content: true
+        }
+      })
+
+      if (!notice) {
+        console.error(`User ${user?.id} tried to non-existent notice ${id}`)
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Notice not found'
+        })
+      }
+
+      return notice
     })
 })
