822611c5d79a22a7a2b269cd96d31c2118b693a5
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-19 19:49:22 +0900

    layout

--- app/(app)/notices/page.tsx
+++ app/(app)/notices/page.tsx
@@ -1,1 +1,17 @@
-export default function NoticesPage() {}
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+
+export default async function NoticesPage() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <hr></hr>
+      <nav>
+        <Link href={`/notices/new`}>
+          <Button>새 글 작성</Button>
+        </Link>
+      </nav>
+      <main></main>
+    </div>
+  )
+}
