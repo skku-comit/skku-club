9b25cc8fce42a8466d91188178030416faca2442
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-20 17:35:05 +0900

    select

--- app/(app)/(auth)/(admin)/admin/clubs/new/page.tsx
+++ app/(app)/(auth)/(admin)/admin/clubs/new/page.tsx
@@ -24,6 +24,15 @@ import {
   FormMessage
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
 import { apiClient } from '@/lib/api/trpc/client'
 import { NewClubSchema } from '@/lib/schemas'
@@ -115,11 +124,22 @@ export default function NewClubPage() {
                           동아리 분류
                         </FormLabel>
                         <FormControl>
-                          <Textarea className="mt-1 block w-full" {...field} />
+                          <Select>
+                            <SelectTrigger>
+                              <SelectValue placeholder="동아리 분류 선택" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectLabel>분류</SelectLabel>
+                                <SelectItem value="central">
+                                  중앙동아리
+                                </SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
                         </FormControl>
                         <FormDescription>
-                          추후에 동아리 임원한테 동아리 설명 수정 권한을 줄 수
-                          있습니다
+                          예시: 중앙동아리, 준동아리{' '}
                         </FormDescription>
                         <FormMessage />
                       </FormItem>
@@ -133,10 +153,22 @@ export default function NewClubPage() {
                     return (
                       <FormItem>
                         <FormLabel className="block font-bold">
-                          동아리 분류
+                          캠퍼스
                         </FormLabel>
                         <FormControl>
-                          <Textarea className="mt-1 block w-full" {...field} />
+                          <Select>
+                            <SelectTrigger>
+                              <SelectValue placeholder="캠퍼스 선택" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectLabel>캠퍼스</SelectLabel>
+                                <SelectItem value="SEOUL">명륜</SelectItem>
+                                <SelectItem value="SUWON">율전</SelectItem>
+                                <SelectItem value="BOTH">공통</SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
                         </FormControl>
                         <FormDescription>
                           추후에 동아리 임원한테 동아리 설명 수정 권한을 줄 수
