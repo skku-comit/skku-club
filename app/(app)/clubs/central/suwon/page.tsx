2e841834162597eaa076fc4452611dfb25ce6dc7
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-20 19:57:13 +0900

    dynamic

--- app/(app)/clubs/central/suwon/page.tsx
+++ app/(app)/clubs/central/suwon/page.tsx
@@ -2,6 +2,8 @@ import { createApiCaller } from '@/lib/api/trpc/server'
 
 import { ClubListPage } from '../../list-page'
 
+export const dynamic = 'force-dynamic'
+
 export default async function SuwonClubsPage() {
   const caller = await createApiCaller()
 
