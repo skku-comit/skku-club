94336a8a26c8acb43b0f2081567cddd82aa0c5b7
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-20 17:59:09 +0900

    ropute

--- app/(app)/(auth)/(admin)/admin/members/page.tsx
+++ app/(app)/(auth)/(admin)/admin/members/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
+import { useSearchParams } from 'next/navigation'
+
 import { AddAdminButton } from './add-admin-button'
 
 export default function SiteMembersPage() {
+  const searchParams = useSearchParams()
+
   return (
     <div className="flex flex-col gap-y-4">
       <div></div>
