5336c75253b237d7e07462ebdaf103e3937f0c35
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-20 17:10:16 +0900

    pages

--- app/(app)/(auth)/admin/clubs/page.tsx
+++ app/(app)/(auth)/admin/clubs/page.tsx
@@ -1,1 +1,17 @@
-export default function ClubsPage() {}
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+
+export default function ClubsPage() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div></div>
+      <div>
+        <Link href={`/admin/clubs/new`}>
+          <Button>동아리 추가</Button>
+        </Link>
+      </div>
+      <div></div>
+    </div>
+  )
+}
