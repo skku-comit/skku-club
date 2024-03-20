bf6a70fe7d22a2adededa442a8cc862fd591f1d1
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-20 19:54:48 +0900

    list page

--- app/(app)/clubs/list-page.tsx
+++ app/(app)/clubs/list-page.tsx
@@ -1,5 +1,13 @@
-import { Card } from '@/components/ui/card'
+import Link from 'next/link'
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 import { ApiOutput } from '@/lib/api/trpc/router'
+import { formatCampus, formatCategory } from '@/lib/format'
 
 export interface ClubListPageProps {
   clubs: ApiOutput['clubs']['listAll']
@@ -9,7 +17,18 @@ export function ClubListPage({ clubs }: ClubListPageProps) {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
       {clubs.map((club) => {
-        return <Card key={club.id.toString()}></Card>
+        return (
+          <Link key={club.id.toString()} href={`/club/${club.id}`}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{club.name}</CardTitle>
+                <CardDescription>
+                  {formatCategory(club.category)} - {formatCampus(club.campus)}
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </Link>
+        )
       })}
     </div>
   )
