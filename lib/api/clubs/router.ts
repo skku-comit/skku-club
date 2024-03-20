f5c07e6f08cdc065b2b8bf83b9f16c03b6f4141a
강동윤/Donny <kdy1997.dev@gmail.com>
2024-03-20 17:36:35 +0900

    fix api

--- lib/api/clubs/router.ts
+++ lib/api/clubs/router.ts
@@ -9,30 +9,37 @@ export const clubs = router({
   create: protectedProcedure
     .input(NewClubSchema)
     .output(ClubSchema.pick({ id: true }))
-    .mutation(async ({ input: { name, description }, ctx: { user } }) => {
-      if (user.role !== 'ADMIN') {
-        throw new TRPCError({
-          code: 'FORBIDDEN',
-          message: '관리자만 동아리를 추가할 수 있습니다.'
-        })
-      }
-
-      const club = await db.club.create({
-        data: {
-          name,
-          description,
-          creator: {
-            connect: {
-              id: user.id
-            }
-          }
-        },
-
-        select: {
-          id: true
+    .mutation(
+      async ({
+        input: { name, description, category, campus },
+        ctx: { user }
+      }) => {
+        if (user.role !== 'ADMIN') {
+          throw new TRPCError({
+            code: 'FORBIDDEN',
+            message: '관리자만 동아리를 추가할 수 있습니다.'
+          })
         }
-      })
 
-      return club
-    })
+        const club = await db.club.create({
+          data: {
+            name,
+            description,
+            category,
+            campus,
+            creator: {
+              connect: {
+                id: user.id
+              }
+            }
+          },
+
+          select: {
+            id: true
+          }
+        })
+
+        return club
+      }
+    )
 })
--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
+import { CampusSchema } from './prisma'
+
 const NoticeTitleSchema = z.string().min(1).max(100)
 
 const NoticeContentSchema = z.string().min(1)
@@ -16,5 +18,7 @@ const ClubDescriptionSchema = z.string().min(1)
 
 export const NewClubSchema = z.object({
   name: ClubTitleSchema,
-  description: ClubDescriptionSchema
+  description: ClubDescriptionSchema,
+  campus: CampusSchema,
+  category: z.string()
 })
